3812d4bdd64b882240e7e5134dd83c805264b650
Ankur Seth <iankurseth@gmail.com>
2022-01-11 16:55:05 +0530

    c0 with CRA and tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="text-purple-800 text-4xl p-12 font-bold">Hello, todo!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
