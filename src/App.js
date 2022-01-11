db2f955392d1e2cc6bb0d6203c31de2bea5a2fcc
Ankur Seth <iankurseth@gmail.com>
2022-01-11 17:31:48 +0530

    Working with ofg, and changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="text-purple-800 text-4xl p-12 font-bold">Hello, todo!</p>
+        <p className="text-purple-800 text-4xl p-12 font-bold">It works!</p>
       </header>
     </div>
   );
